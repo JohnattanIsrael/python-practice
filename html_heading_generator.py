1378d56397535dd9098a10ce851fc4c6870211d3
JohnaAngeles <johnattanisraelangeles@gmail.com>
2020-07-04 12:13:53 -0500

    0407 2020

--- html_heading_generator.py
+++ html_heading_generator.py
@@ -19,4 +19,42 @@
 </body>
 </html>
 """
-def heading_generator(title, meta_name, content, h1)
+def heading_generator(title, heading_type):
+    return f'''<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>{title}</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+  <h1>{heading_type}</h1>
+</body>
+</html>'''
+
+print(heading_generator('You did it bro', 'YOU ACTUALLY DID IT'))
+print(heading_generator('hi there', '3'))
+
+print('-----------')
+
+def standard_form(name, lastname, email, phone):
+    return f'''My name is {name} 
+    please contact me at my email: {email}
+    
+    Or at my phone number {phone}
+
+    thank you
+    {name} {lastname}
+    '''
+print(standard_form('Mario', 'Pineda', 'email@email . com', '555 555 555'))
+
+
+
--- lists_in_python.py
+++ lists_in_python.py
@@ -1,21 +1,61 @@
-tags = ['python', 'development', 'tutorials', 'code']
+# tags = ['python', 'development', 'tutorials', 'code']
 
-# in this case you replaced the last element so it´s not for adding elements
-# tags[-1] = 'Programming'
-# print(tags) #> ['python', 'development', 'tutorials', 'Programming']
+# # in this case you replaced the last element so it´s not for adding elements
+# # tags[-1] = 'Programming'
+# # print(tags) #> ['python', 'development', 'tutorials', 'Programming']
 
-# adding without brackets divides each character
-# tags.extend('Programming')
-# print(tags) #> ['python', 'development', 'tutorials', 'code', 'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+# # adding without brackets divides each character
+# # tags.extend('Programming')
+# # print(tags) #> ['python', 'development', 'tutorials', 'code', 'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
 
-#so this is the one way to add an elemnt to the end f the list BUTit does not store a new value, just like the sort function
-tags.extend(['Programming']) #if you place this in a variable it will return = none
-print(tags) #> ['python', 'development', 'tutorials', 'code', 'Programming']
+# #so this is the one way to add an elemnt to the end f the list BUTit does not store a new value, just like the sort function
+# tags.extend(['Programming']) #if you place this in a variable it will return = none
+# print(tags) #> ['python', 'development', 'tutorials', 'code', 'Programming']
 
-tags = ['python', 'development', 'tutorials', 'code']
+# tags = ['python', 'development', 'tutorials', 'code']
 
-new_tags = tags + ['Programming']
+# new_tags = tags + ['Programming']
 
-print(new_tags) #>['python', 'development', 'tutorials', 'code', 'Programming']
+# print(new_tags) #>['python', 'development', 'tutorials', 'code', 'Programming']
 
+num_list = range(1, 11)
+cubed_nums = []
 
+# Normal way
+for num in num_list:
+    cubed_nums.append(num ** 3)
+
+print(list(num_list))#> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(cubed_nums) #>[1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+
+print('----List comprehention------')
+
+num_list = range(1, 11)
+cubed_nums = []
+
+cubed_nums = [num ** 3 for num in num_list]
+
+print(list(num_list))#> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(cubed_nums) #>[1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+
+print('more--------another normal example, with a conditional ')
+
+num_list = range(1, 11)
+
+even_numbers = []
+
+for num in num_list:
+    if num % 2 == 0:
+        even_numbers.append(num)
+
+print(list(num_list))#> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(even_numbers) #>[2, 4, 6, 8, 10]
+
+print('more--------another list comprehention example,with a conditional ')
+
+num_list = range(1, 11)
+
+even_numbers = [num for num in num_list if num % 2 == 0]
+
+print(list(num_list))#> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(even_numbers) #>[2, 4, 6, 8, 10]
