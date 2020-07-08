a1ffff40599c0b898224dc68989470fa3ddddd6c
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-07-08 18:15:10 -0500

    different importing options

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+# import sys
+# sys.path.insert(0, './libs')
+# import helper
+
+# def render():
+#     print(helper.greeting('Elizabeth', 'Martinez'))
+
+# render()
+
+# Import a n specific function
+
+# import sys
+# sys.path.insert(0, './libs')
+# from helper import greeting
+
+# def render():
+#     print(greeting('Elizabeth', 'Martinez'))
+
+# render()
+
+
+#import and assign an alias 
+
 import sys
 sys.path.insert(0, './libs')
-import helper
+import helper as h
 
 def render():
-    print(helper.greeting('Elizabeth', 'Martinez'))
+    print(h.greeting('Elizabeth', 'Martinez'))
 
 render()
+
