40aeebe8f6b87761964d185634bd019cf4eefa7a
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-07-08 18:07:41 -0500

    import modules and add paths

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -30,3 +30,27 @@ def fizzbuzz(variables, *args):
 
 variables = {3:'Fizz',5:'Buzz'}
 fizzbuzz(variables,101)
+
+#I like this one too from Jordan Hudgens
+
+"""
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the
+number and for the multiples of five print “Buzz”. For
+numbers which are multiples of both three and five print
+“FizzBuzz”.
+"""
+
+def fizz_buzz(max_num):
+  for num in range(1, max_num + 1):
+    if num % 3 == 0 and num % 5 == 0:
+      print('FizzBuzz')
+    elif num % 5 == 0:
+      print('Buzz')
+    elif num % 3 == 0:
+      print('Fizz')
+    else:
+      print(num)
+
+
+fizz_buzz(100)
---
+++ modules.py
@@ -0,0 +1,6 @@
+#Python Package Index
+#Pypi
+#Cheese Shop
+#Remeber that you connect the libraries and Modules in Pypi using PiP
+
+
