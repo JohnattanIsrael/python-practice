ef3273545b9a881e2d438669b8479a0786fc22be
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-07-17 21:54:50 -0500

    now we have started the first part of a json API

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os
@@ -34,6 +34,28 @@ class GuideSchema(ma.Schema):
 guide_schema= GuideSchema()
 guides_schema = GuideSchema(many=True)
     
+# Create a POST API Endpoint - to create a new guide
+# if you want to create a new guide you are going to point to this endpoint and use the http POST method at the /guide route- now we will create a method
+@app.route('/guide', methods=['POST'])
+def add_guide():
+    title = request.json['title'] #so this is requesting from the database, and will be like a dictionay lookup , will parse it and store it in the variable(the Title obviously)
+    content = request.json['content']#this means we will have acces to the totle and the content
+
+    new_guide = Guide(title, content) #this is instabtiatin from the past lines, storing it in the new guide variable
+# we are creating a new database session and is going to add that new guide onside of it,
+    db.session.add(new_guide)
+    # this commit thing is from the sqlachemy we are opening up a new conectiont to the database and we want to add info inside of it
+    db.session.commit()
+
+    #now lets make sure that it works, by queyngs, is a bad practice to create a guide without returning something
+    # this wiil look up for the new guide id and will store it in the guide variable
+    guide = Guide.query.get(new_guide.id)
+    # this will return the ghide_schema but converted to a json file,(the guide variable.)
+    return guide_schema.jsonify(guide)
+# THIS IS A JSON API
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
